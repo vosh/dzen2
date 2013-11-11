58cb07722cb70a7bb33986121281721eb5caebb7
vosh <bdrolshagen@gmail.com>
2013-11-11 11:51:33 +0100

    dzen2 bar scripts

---
+++ mixer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wmname "spectrwm"
+setxkbmap de nodeadkeys
+vol=`amixer get Master | grep "Front Left:" | awk '{print $5}' | tr -d '[]'`
+    if [ $vol == "0%" ]
+    then
+echo "Mute"
+    else
+echo $vol
+    fi
+else
+echo "Mute"
+fi
